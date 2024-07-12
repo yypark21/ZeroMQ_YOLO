94968c30586be0a7e615541cb45f2530ae1980b3
YY Park <96404926+yypark21@users.noreply.github.com>
2024-07-12 10:37:32 +0900

    Cpp 솔루션 추가

---
+++ cpp/ZeroMQ_Version/main.cpp
@@ -0,0 +1,32 @@
+﻿#include "main.h"
+#include "Sender.h"
+#include "Receiver.h"
+
+int main()
+{	
+	ZeroMQ::Sender sender;
+	sender.SetPCIP("tcp://*:5555");
+	sender.SetImageAddress("../../image/F 1_1.tif");
+
+	int count = 0;
+	
+	while (1) {
+		// Reading the image through opencv package
+		std::cout << count << std::endl;
+		sender.SendImage();
+		count += 1;
+	}
+
+	/*
+	ZeroMQ::Receiver receiver;
+
+	while (1) {
+		// Reading the image through opencv package
+		//std::cout << count << std::endl;
+		receiver.ReceiveImage();
+		//std::count += 1;
+	}
+	*/
+
+	return 0;
+}
---
+++ cpp/ZeroMQ_Version/main.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+#ifdef _WIN32
+#include<Windows.h>
+#elif defined __unix__
+#include <unistd.h>
+#endif
